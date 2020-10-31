b380b20f732d1c0a37a6434a962d894af002fbae
lisagames <73744447+lisagames@users.noreply.github.com>
2020-10-31 17:34:08 +0200

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1604156875,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/aimspot-sheet0.png",
+		"images/peer-sheet0.png",
+		"images/peerlaser-sheet0.png",
+		"images/sprite-sheet0.png",
+		"media/boom1.m4a",
+		"media/boom1.ogg",
+		"media/start.m4a",
+		"media/start.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"c2mp-net.js",
+		"c2mp-peer.js",
+		"c2mp.js",
+		"waker.js"
+	]
+}
